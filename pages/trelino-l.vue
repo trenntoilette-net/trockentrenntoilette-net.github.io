b48c3ec32d9bd13a688e218a2d9a535ff4fbc1fc
trenntoilette-net <trockentrenntoilette-com@protonmail.com>
2022-03-02 15:25:25 +0100

    trelino l

---
+++ pages/trelino-l.vue
@@ -0,0 +1,257 @@
+<template>
+  <div>
+    <ProductHeader
+      productName="Trelio L mit HPL Veredelung"
+      textContent="Die Trelino L ist eine Trockentoilette aus dem Hause Trelino. Sie ist aus Birkenholz angefertigt und mit robustem HPL veredelt."
+      productImage="/assets/images/trockentrenntoiletten/trockentrenntoilette-trelino-l-hpl-veredelung.webp"
+    ></ProductHeader>
+
+    <!-- About Start -->
+    <div class="container-xxl py-5" id="testbericht">
+      <div class="container">
+        <div class="row g-5">
+          <div class="col-lg-8 fadeInUp" style="min-height: 400px">
+            <h2 class="mb-4">Testbericht</h2>
+            <p class="mb-4">
+              Die Trelino L ist eine Trockentrenntoilette, welche für den mobilen Einsatz konzipiert ist. Sie ist sofort einsatzbereit und ideal für den mobilen Einsatz wie Wohnmobile, Kastenwagen, Tiny Häuser, beim Camping oder auch für das Gartenhaus.
+            </p>
+
+            <h5 class="mb-3 mt-5">Komfort</h5>
+
+            <p>Die Trelino L hat einen ausgezeichneten Sitzkomfort und steht, wie auch bei der <NuxtLink to="/trobolo-terablom/" title="TROBOLO TeraBlœm im Test">TROBOLO TeraBlœm</NuxtLink>, einer herkömmlichen Toilette aus Keramik nichts nach. Die Trockentrenntoilette ist sehr kompakt, hat aber eine normale Toilettensitz-Größe. Durch das HPL veredelte Birkenholz hatten wir stehts ein sehr gutes Sitzgefühl.</p>
+
+            <h5 class="mb-3 mt-5">Eckdaten</h5>
+
+            <p>
+              Die mobile Trelino L Trockentrenntoilette hat ein Gewicht von 8,2 kg und eine Belastbarkeit von 150 kg. Sie ist 316 mm breit, 386 mm tief und 468 mm hoch. Die Sitzhöhe beträgt 42,5 cm. Der Urinkanister umfasst ein Aufbewahrungsvermögen von 10 Litern und der Feststoffbehälter ein Aufbewahrungsvermögen von 10,6 Litern. Der Trenneinsatz ist aus hochglänzender ABS (Acrylnitril-Butadien-Styrol) Oberfläche mit säurebeständiger PMMA-Beschichtung. Der Behälter besteht aus Polyethylen (PE), ist recyclebar und ebenfalls säurebeständig.
+            </p>
+
+            <h5 class="mb-3 mt-5">
+              Funktionsweise, Material und Geruchsentwicklung
+            </h5>
+
+            <p>
+              Die Trelino L Trockentrenntoilette funktionert über einen Trenneinsatz. Urin- und Feststoffbehälter sind voneinander getrennt. Hierdurch wird auch die Geruchsentwicklung verhindert. Die Trelino L benötigt kein Wasser oder Chemie. Somit ist sie überall sofort einsetzbar. Auch aufgrund der kompakten Größe und des Gewichtes eignet sich diese mobile Trockentrenntoilette ideal für Anwendungen wie im Wohnmobil, als Campingtoilette oder für den Einsatz im Gartenhäuschen. Die verwendeten Materialien sind qualitativ und erwecken einen robusten und beständigen Eindruck. Die Scharniere des Toilettendeckels sind aus Edelstahl und somit vor Oxidation geschützt. Das Birkenholz besteht aus mehreren Schichten und erzeugt ebenfalls einen qualitativen und robusten Eindruck.
+            </p>
+
+            <p>
+              Die Entleerung hat sich als einfach erwiesen, dennoch etwas schwieriger als bei der TeraBlœm von TROBOLO. Aufgrund 
+            </p>
+
+            <h5 class="mb-3 mt-5">Nachhaltigkeit</h5>
+
+            <p>
+              Laut Trelino haben sie es sich zur Aufgabe gemacht, wertvolle Ressourcen zu sparen und die Natur zu schonen. Durch die Nutzung einer Trockentrenntoilette entfällt die Notwendigkeit von Wasser und Chemie, was pauschal der Umwelt zu Gute kommt.
+            </p>
+
+            <p>
+              Trelino nutzt für die Herstellung der Trelino L recyclefähige Materialien um die Produktion nachhaltig zu gestalten. Die Produktion findet in Deutschland statt und hat somit kurze Lieferungswege. Trelino stellt Produkte mit hoher Qualität her, welche eine lange Lebensdauer aufweisen und somit kein "Wegwerfprodukt" sind. 
+            </p>
+
+            <h5 class="mb-3 mt-5">Zubehör</h5>
+
+            <p>
+              Als optionales Zubehör bietet Trelino ein elektrisches Abluftsystem an um der Geruchsentwicklung entgegenzuwirken.
+            </p>
+
+            <h5 class="mb-3 mt-5">Fazit</h5>
+
+            <p>
+              Die Trelino L mit HPL Veredelung ist eine qualitative, kompakte Trockentrenntoilette, welche sich herrvorragend für den mobilen Einsatz eignet. Durch die HPL Veredelung haben wir hier ein Produkt welches sehr Nachhaltig ist. Die Wasserbeständigkeit, das mehrschichtige Birkenholz und die festen Scharniere aus Edelstahl, sowie aber auch die Einfachheit der Entleerung und Hygiene, machen die Trelino L zu einem optimalem Reisebegleiter.
+            </p>
+
+            <p>
+              Aufgrund der Mobilität, der Funktionaltiät, der Nachhaltigkeit
+              des Produktes, der Einfacheit der Entleerung sowie der Hygiene,
+              vergeben wir der Trelino L von Trelino eine Bestnote in der Kategorie mobile Trockentrenntoiletten.
+            </p>
+
+            <div class="row gy-2 gx-4 mb-5 mt-5">
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Funktionalität
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Materialien
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Nachhaltigkeit
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Geruchsentwicklung
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Komfort
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Entsorgung & Hygiene
+                </p>
+              </div>
+            </div>
+            <a
+              class="btn btn-primary py-3 px-5"
+              target="_blank"
+              rel="nofollow noopener"
+              href="https://www.meinetrenntoilette.de/collections/zubehoer-fuer-trenntoilette/products/trelino-luefter-zubehoer-set-trenntoilette"
+              >Bestellen</a
+            >
+          </div>
+          <div class="col-lg-4 fadeInUp">
+            <div>
+              <div class="sidebar-widget schedule-widget mt-5">
+                <h5 class="mb-4">Trelino L mit HPL Veredelung</h5>
+                <ul class="list-unstyled">
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Marke</a> <span>Trelino</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Material</a>
+                    <span>Mehrschichtiges Birkenholz</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Maße</a>
+                    <span>Höhe 468 - Tiefe 386 - Breite 316</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Gewicht</a> <span>8,2 kg</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Traglast</a> <span>150 kg</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Funktion</a> <span>Trennsystem</span>
+                  </li>
+                </ul>
+                <div class="sidebar-contatct-info mt-4">
+                  <p class="mb-0">Preis</p>
+                  <h3>799,00 EUR</h3>
+                </div>
+              </div>
+            </div>
+            <div>
+              <!-- <img
+                class="img-fluid"
+                src="/assets/images/trockentrenntoiletten/trockentrenntoilette-meinetrenntoilette-trelino-l.webp"
+                alt="TROBOLO LuweBlœm"
+              /> -->
+            </div>
+            <div class="mt-5">
+              <a
+                class="btn btn-primary py-3 px-5"
+                target="_blank"
+                rel="nofollow noopener"
+                href="https://www.meinetrenntoilette.de/collections/zubehoer-fuer-trenntoilette/products/trelino-luefter-zubehoer-set-trenntoilette"
+                style="display: block; width: 100%"
+                >Trelino Online Shop</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About End -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Trelino L mit HPL Veredelung",
+  head: {
+    title: "Trelino L mit HPL Veredelung im Test",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Trockentrenntoilette Trelino L mit HPL Veredelung im Test. Vorteile und Nachteile. Kriterien: Geruchtsentwicklung, Komfort, Hygiene, Entlerrung und Preis.",
+      },
+    ],
+  },
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@graph": [
+        {
+          "@context": "https://schema.org/",
+          "@type": "Product",
+          name: "Trelino L mit HPL Veredelung",
+          image: [
+            "https://www.trockentrenntoilette.net/assets/images/trockentrenntoiletten/trockentrenntoilette-trelino-l-hpl-veredelung.webp",
+          ],
+          description:
+            "Trelino L - Trockentrenntoilette mit HPL Veredelung für den mobilen Einsatz wie Camping, Tiny Häuser, Wohnmobile und Vans.",
+          sku: "T1-02502",
+          // mpn: "24542",
+          brand: {
+            "@type": "Brand",
+            name: "Trelino",
+          },
+          review: {
+            "@type": "Review",
+            reviewRating: {
+              "@type": "Rating",
+              ratingValue: "4",
+              bestRating: "4",
+            },
+            author: {
+              "@type": "Organization",
+              name: "Trockentrenntoilette.net",
+            },
+          },
+          aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: "4.2",
+            reviewCount: "122",
+          },
+          offers: {
+            "@type": "Offer",
+            url: "https://www.trockentrenntoilette.net/trelino-l/",
+            priceCurrency: "EUR",
+            price: "799.00",
+            priceValidUntil: "2024-11-20",
+            itemCondition: "https://schema.org/NewCondition",
+            availability: "https://schema.org/InStock",
+          },
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              item: {
+                "@id": "https://www.trockentrenntoilette.net/",
+                name: "Trockentrenntoiletten"
+              }
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              item: {
+                "@id": "https://www.trockentrenntoilette.net/trelino-l/",
+                name: "Trelino L"
+              }
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
