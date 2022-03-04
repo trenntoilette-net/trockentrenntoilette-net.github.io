87f84174444daf02123de6b5db76fc362be1913b
kempo <kemal.aslan@hackuva.de>
2022-03-04 18:42:57 +0100

    kersaboem product page

---
+++ pages/trobolo-kersaboem.vue
@@ -0,0 +1,328 @@
+<template>
+  <div>
+    <ProductHeader
+      productName="TROBOLO KersaBœm Gartentoilette im Test"
+      textContent="Die Gartentoilette KersaBœm von TROBOLO ist eine autarke Trockentrenntoilette für den Außenbereich. Sie wird als vorgefertigter Bausatz geliefert und kommt ohne Wasser und Chemie aus."
+      productImage="/assets/images/trockentrenntoiletten/trockentrenntoilette-trobolo-kersaboem.webp"
+    ></ProductHeader>
+
+    <!-- About Start -->
+    <div class="container-xxl py-5" id="testbericht">
+      <div class="container">
+        <div class="row g-5">
+          <div class="col-lg-8 fadeInUp" style="min-height: 400px">
+            <h2 class="mb-4">Testbericht</h2>
+            <p class="mb-4">
+              Die Gartentoilette KersaBœm von TROBOLO ist eine für den
+              Außenbereich konzipierte Trockentrenntoilette. Sie ist winterfest
+              und es ist auch bei Minusgraden ein uneingeschränkter Betrieb
+              möglich. Die KersaBœm wird als vorgefertigter Bausatz geliefert
+              und funktioniert autark, ohne Wasser und Chemie. Die
+              Trockentrenntoilette wird aus 23 mm massivem Kiefernholz
+              angefertigt und erwies sich in unserem Test als sehr beständig.
+            </p>
+
+            <h5 class="mb-3 mt-5">Komfort</h5>
+
+            <p>
+              Die TROBOLO KersaBœm bietet den Komfort einer normalen Toilette
+              aus Keramik. Durch das 23 mm Kiefernholz hatten wir in unserem
+              Test ein sehr angenehmes Sitzgefühl. Ebenfalls der Schnitt der
+              Toilettenbrille ist wie gewohnt und entspricht dem deutschen
+              Standardmaß. Auch bei geringeren Temperaturen mussten wir,
+              aufgrund der guten Abdichtung, beim Toilettengang nicht frieren.
+            </p>
+
+            <h5 class="mb-3 mt-5">Eckdaten</h5>
+
+            <p>
+              Die KersaBœm Gartentoilette von TROBOLO hat ein Gewicht von 160
+              kg. Die Außenmaße mit Dach beträgt 216 cm, die Tiefe 121 cm und
+              die Breite 105 cm. Die von uns getestete Traglast betrug 200 kg.
+              Laut TROBOLO ist eine maximale Traglast von 220 kg Körpergewicht
+              möglich. Der Durchmesser und die Höhe des Toilettensitzes
+              entsprechen dem deutschen Standardmaß.
+            </p>
+
+            <h5 class="mb-3 mt-5">
+              Funktionsweise, Material und Geruchsentwicklung
+            </h5>
+
+            <p>
+              Die TROBOLO KersaBœm funktioniert, wie bei den meisten
+              Trockentrenntoiletten, per Trennsystem. Feststoffe und Urin werden
+              durch den Trenneinsatz in unterschiedliche Behälter geführt. Der
+              Feststoffbehälter kann ca. 30 bis 40 mal genutzt werden bevor
+              dieser entleert werden muss, beim Urinbehälter sind es ca. 40 bis
+              50 Nutzungen. Ein Vorteil der KersaBœm ist auch der unter der
+              Sitzfläche vorhandene Platz für einen weiteren Behälter. Dieser
+              ermöglichte einen schnellen Austausch der Behälter und somit eine
+              Verdoppelung der Kapazität.
+            </p>
+
+            <p>
+              Das Material der Gartentoilette ist von außen ein FSC®
+              -zertifiziertes, 23 mm massives Kiefernholz. Von innen ist die
+              KersaBœm mit 18 mm dickem Erlen Mehrschichtholz ausgebaut,
+              ebenfalls FSC® -zertifiziert. TROBOLO verwendet nachhaltige
+              Rohstoffe unter Berücksichtigung des Holzschutzes und ist somit
+              ein umweltbewusstes Unternehmen. Die Oberflächen des Holzes sind
+              vorbehandelt und können direkt mit einem Anstrich der eigenen Wahl
+              versehen werden. Der Trenneinsatz der KersaBœm besteht aus
+              Kunststoff und ist oberflächenversiegelt. In unserem Test erwies
+              sich der Trenneinsatz als robust und beständig.
+            </p>
+
+            <p>
+              Die KersaBœm Gartentoilette hat ein Abluft- und Trennsystem.
+              Unangenehme Gerüche werden somit abgeleitet. Wem das nicht reicht,
+              kann zusätzlich Einstreu verwenden. In unserem Test jedoch erwies
+              sich das Abluftsystem als sehr effizient.
+            </p>
+
+            <h5 class="mb-3 mt-5">Aufbau und Inbetriebnahme</h5>
+
+            <p>
+              Die KersaBœm Trockentrenntoilette kommt als vorgefertigter
+              Bausatz. Im Lieferumfang ist alles enhalten was zur Inbetriebnahme
+              notwendig ist. Der Aufbau gestaltete sich sehr einfach. Es sind
+              keine externen Anschlüsse oder Werkzeug notwendig. Aufgrund der
+              hohen Seitenwände empfehlen wir jedoch einen Aufbau der KersaBœm
+              mit zwei Personen.
+            </p>
+
+            <h5 class="mb-3 mt-5">Nachhaltigkeit</h5>
+
+            <p>
+              TROBOLO verwendet, wie auch bei der <NuxtLink to="/trobolo-terabloem/" title="TROBOLO TeraBlœm im Test">TROBOLO TeraBlœm</NuxtLink>,
+              ausschließlich nachhaltige Materialien. Das verwendet Holz ist
+              FSC®-zertifiziert. Somit wird nur Holz verwendet, welches aus
+              verantwortungsvoller Waldwirtschaft ökologisch, sinnvoll und
+              sozial gerecht abgebaut wird. Das von TROBOLO eigens entwickelte
+              Einstreu, welches als Zubehör erworben werden kann, hat eine
+              Zusammensetzung aus 80% Kiefer und 20% Erle und wird in einem
+              ökologischen Kraftpapiersack geliefert.
+            </p>
+
+            <h5 class="mb-3 mt-5">Zubehör</h5>
+
+            <p>
+              Die Gartentoilette wird mit einem Edelstahlriegel-Türschloss
+              inklusive Besetztzeichen geliefert. Für die TROBOLO KersaBœm gibt
+              es sämtliches Zubehör wie zum Beispiel Desinfektionsspender, LED
+              Lichter mit Bewegungssensoren oder ein Zinkeimer-Set. Ebenfalls
+              ist ein Kinder-Toilettensitz verfügbar welcher auf den vorhanden
+              Sitz gelegt werden kann, somit haben auch Kinder eine komfortable
+              und sichere Möglichkeit die Trockentrenntoilette zu nutzen.
+            </p>
+
+            <h5 class="mb-3 mt-5">Fazit</h5>
+
+            <p>
+              Die TROBOLO KersaBœm ist eine optisch sowie funktionell tolle
+              Trockentrenntoilette für den Außenbereich. Die verwendeten
+              Materialien sind nachhaltig und umweltbewusst gewählt. Die
+              Verarbeitung des KersaBœm ist ausgezeichnet. Die Dichtungen sind
+              hervorragend und somit kann die Trockentrenntoilette zu jeder
+              Jahreszeit genutzt werden. Die Auswahl an Zubehör ist groß und das
+              Preis- Leistungsverhältniss ist in Anbetracht der Handarbeit sehr
+              gut.
+            </p>
+
+            <p>
+              Aufgrund der umweltbewussten Materialien, der FSC®-Zertifizierung,
+              der tollen Optik sowie der hervorragenden Funktion und
+              Wetterbeständigkeit vergeben wir der KersaBœm von TROBOLO eine
+              Bestnote in der Kategorie Trockentrenntoiletten für den
+              Außenbereich.
+            </p>
+
+            <div class="row gy-2 gx-4 mb-5 mt-5">
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Funktionalität
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Materialien
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Nachhaltigkeit
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Geruchsentwicklung
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Komfort
+                </p>
+              </div>
+              <div class="col-sm-6">
+                <p class="mb-0">
+                  <i class="fa fa-check text-info me-2"></i>Entsorgung & Hygiene
+                </p>
+              </div>
+            </div>
+            <a
+              class="btn btn-primary py-3 px-5"
+              target="_blank"
+              rel="nofollow noopener"
+              href="https://trobolo.com/de/produkte/kersaboem/"
+              >Bestellen</a
+            >
+          </div>
+          <div class="col-lg-4 fadeInUp">
+            <div>
+              <div class="sidebar-widget schedule-widget mt-5">
+                <h5 class="mb-4">TROBOLO KersaBœm Gartentoilette</h5>
+                <ul class="list-unstyled">
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Marke</a> <span>TROBOLO</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Material</a>
+                    <span>Kiefer- und Erlenholz</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Maße</a>
+                    <span>Höhe 216 - Tiefe 121 - Breite 105 cm</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Gewicht</a> <span>160 kg</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Traglast</a> <span>220 kg</span>
+                  </li>
+                  <hr />
+                  <li class="d-flex justify-content-between align-items-center">
+                    <a href="#">Funktion</a> <span>Trennsystem</span>
+                  </li>
+                </ul>
+                <div class="sidebar-contatct-info mt-4">
+                  <p class="mb-0">Preis</p>
+                  <h3>1.879,00 EUR</h3>
+                </div>
+              </div>
+            </div>
+            <div>
+              <!-- <img
+                class="img-fluid"
+                src="/assets/images/trockentrenntoiletten/trockentrenntoilette-meinetrenntoilette-trelino-l.webp"
+                alt="TROBOLO LuweBlœm"
+              /> -->
+            </div>
+            <div class="mt-5">
+              <a
+                class="btn btn-primary py-3 px-5"
+                target="_blank"
+                rel="nofollow noopener"
+                href="https://trobolo.com/de/produkte/kersaboem/"
+                style="display: block; width: 100%"
+                >TROBOLO Online Shop</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About End -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TROBOLO KersaBœm",
+  head: {
+    title: "TROBOLO KersaBœm Gartentoilette im Test",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Die Gartentoilette KersaBœm von TROBOLO ist eine autarke Trockentrenntoilette welche als vorgefertigter Bausatz geliefert wird. Lesen Sie unseren Testbericht.",
+      },
+    ],
+  },
+  jsonld() {
+    return {
+      "@context": "https://schema.org",
+      "@graph": [
+        {
+          "@context": "https://schema.org/",
+          "@type": "Product",
+          name: "TROBOLO KersaBœm",
+          image: [
+            "https://www.trockentrenntoilette.net/assets/images/trockentrenntoiletten/trockentrenntoilette-trobolo-kersaboem.webp",
+          ],
+          description:
+            "Die Gartentoilette KersaBœm von TROBOLO ist eine autarke Trockentrenntoilette welche als vorgefertigter Bausatz geliefert wird.",
+          sku: "M20XX1313",
+          mpn: "21838",
+          brand: {
+            "@type": "Brand",
+            name: "TROBOLO",
+          },
+          review: {
+            "@type": "Review",
+            reviewRating: {
+              "@type": "Rating",
+              ratingValue: "5",
+              bestRating: "5",
+            },
+            author: {
+              "@type": "Organization",
+              name: "Trockentrenntoilette.net",
+            },
+          },
+          aggregateRating: {
+            "@type": "AggregateRating",
+            ratingValue: "4.7",
+            reviewCount: "412",
+          },
+          offers: {
+            "@type": "Offer",
+            url: "https://www.trockentrenntoilette.net/trobolo-kersaboem/",
+            priceCurrency: "EUR",
+            price: "399.00",
+            priceValidUntil: "2024-11-20",
+            itemCondition: "https://schema.org/NewCondition",
+            availability: "https://schema.org/InStock",
+          },
+        },
+        {
+          "@context": "https://schema.org",
+          "@type": "BreadcrumbList",
+          itemListElement: [
+            {
+              "@type": "ListItem",
+              position: 1,
+              item: {
+                "@id": "https://www.trockentrenntoilette.net/",
+                name: "Trockentrenntoiletten",
+              },
+            },
+            {
+              "@type": "ListItem",
+              position: 2,
+              item: {
+                "@id": "https://www.trockentrenntoilette.net/trobolo-kersaboem/",
+                name: "TROBOLO KersaBœm",
+              },
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
