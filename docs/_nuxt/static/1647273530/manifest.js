cb8776563ae22a94fdd1bbee992ba6884479ec45
kempo <kemal.aslan@hackuva.de>
2022-03-14 17:17:00 +0100

    deploying

--- docs/_nuxt/static/1647273530/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fhersteller\u002Fkildwick","\u002Fcampingtoilette","\u002Fkildwick-miniloo","\u002Ftrobolo-kersaboem","\u002Fgartentoilette","\u002Ftrobolo-terabloem","\u002Finnenbereich","\u002Ftrelino-l","\u002Fhersteller\u002Ftrelino","\u002Fhersteller\u002Ftrobolo"]})
