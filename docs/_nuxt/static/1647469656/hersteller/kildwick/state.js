dcfc2ac011f33fd3177ea013d085685b17d58259
kempo <kemal.aslan@hackuva.de>
2022-03-16 23:27:39 +0100

    deploying

---
+++ docs/_nuxt/static/1647469656/hersteller/kildwick/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647469656",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhersteller\u002Fkildwick\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
