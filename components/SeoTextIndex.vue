276d01a0315ae490564b098043fea85db1651e4b
trenntoilette-net <trockentrenntoilette-com@protonmail.com>
2022-08-08 21:11:50 +0200

    deploying

--- components/SeoTextIndex.vue
+++ components/SeoTextIndex.vue
@@ -1,76 +1,109 @@
 <template>
   <div class="container pt-5">
-      <div class="row g-5">
-        <div class="col-lg-8 offset-lg-2 fadeInUp" style="min-height: 400px">
-          <div class="mb-4 h2">
-            Trocken- und Trenntoiletten: Hersteller, Produkte & Ratgeber
-          </div>
-          <p class="mb-4">
-            Trockentrenntoiletten erfreuen sich überall, wo wasserführende Sanitärinstallationen nicht möglich, zunehmender Beliebtheit:
-          </p>
-          <p>
-            <ul>
-              <li>im Wohnmobil</li>
-              <li>im Gartenhaus</li>
-              <li>im Wochenend- oder Ferienhaus.</li>
-            </ul>
-          </p>
-          <p class="mb-4">
-            Wo Menschen zusammenkommen oder länger verweilen, ist es gut, eine Toilette zu besitzen. Doch nicht immer lässt die Infrastruktur in abgelegenen Regionen, auf Naturgrundstücken, in Wasserschutzgebieten oder unterwegs Sanitärinstallationen zu.
-          </p>
-          <p class="mb-4">
-            Trockentrenntoiletten sind hier die optimale Lösung - sie kommen ganz ohne Rohrleitungssystem und Wasseranschluss aus.
-          </p>
-          <p class="mb-4">
-            Moderne Trenn- und Trockentoiletten sind die umweltfreundliche, hygienische und geruchsneutrale Alternative zu chemischen Toiletten.
-          </p>
-          <div class="mb-4 h2 pt-5">Der Unterschied zwischen Trenn- und Trockentoiletten</div>
-          <p class="mb-4">
-            Der Begriff Trockentrenntoilette setzt sich aus den Bezeichnungen Trockentoilette und Trenntoilette zusammen:
-          </p>
-          <p class="mb-4 h4">
-            Was ist eine Trockentoilette?
-          </p>
-          <p class="mb-4">
-            Wie es der Name bereits andeutet: Trockentoiletten kommen ganz ohne Wasser aus.
-          </p>
-          <p class="mb-4">
-            Ansonsten verfügen auch diese Toiletten über einen Sitz, einen Deckel und eine Toilettenschüssel. Über bestimmte Systeme landen entweder alle Hinterlassenschaften in einem Behälter, der dann geleert werden muss – oder es findet eine Trennung statt. Hier beginnt bereits der Übergang zur Trenntoilette.
-          </p>
-          <div class="mb-4 h4">Was zeichnet eine Trenntoilette aus?</div>
-          <p class="mb-4">
-            Hat eine Trockentoilette getrennte Behälter für Gelbes und Braunes, ist sie gleichzeitig auch eine Trenntoilette.
-          </p>
-          <p class="mb-4">
-            Die Trennung ist sinnvoll, da die Vermischung von Ausscheidungen zu unangenehmen Gerüchen führen kann. Da Menschen wesentlich mehr Urin ausscheiden und dieser nicht ganz so schnell unangenehm riecht, kann der Behälter in anderen Intervallen entsorgt werden. Während Urin in die Natur gegossen werden kann, werden feste Ausscheidungen in der Regel anders entsorgt.
-          </p>
-          <p class="mb-4">
-            In vielen Trenntoiletten wird Festes zusätzlich mit etwas Naturmaterial abgedeckt, wodurch Gerüche deutlich reduziert werden.
-          </p>
-
-          <div class="mb-4 h4">Geeignet sind unter anderem:</div>
-
-          <p class="mb-4">
-            <ul>
-              <li>Kokosfaser</li>
-              <li>Rindenmulch</li>
-              <li>Erde</li>
-              <li>Katzenstreu</li>
-              <li>Holzspäne</li>
-            </ul>
-          </p>
-
-          <div class="mb-4 h5">Merke: Eine Trockentrennoilette ist eine Kombination aus Trockentoilette und Trenntoilette. Sie kommt ohne Wasser aus und hat getrennte Auffangvorrichtungen für Urin und Kot.</div>
-
-          <div class="mb-4 h2 pt-5">Worauf muss beim Kauf einer Trockentrenntoilette geachtet werden?</div>
-
-          <p class="mb-4">Wichtigste Kaufkriterien sind die Größe, Funktionalität, die Art der Behältnisse, Entnahme-, Reinigungs- und Entsorgungsmöglichkeiten sowie das Zubehör.
-Trenntoiletten können kleine Vollkunststoff-Konstruktionen sein, die großen Mülleimern ähneln. Es gibt Trockentoiletten in Holzverkleidungen, fertige Klohäuschen, Camping-Trenntoiletten oder Minitoiletten.</p>
-
-          <p class="lead mb-4">
-            Bei uns finden Sie die beliebtesten Modelle, dazu Tipps und Tricks sowie ausführliche Kaufratgeber zum Thema Trockentoilette und Trenntoilette.
-          </p>
+    <div class="row g-5">
+      <div class="col-lg-8 offset-lg-2 fadeInUp" style="min-height: 400px">
+        <div class="mb-4 h2">
+          Trocken- und Trenntoiletten: Hersteller, Produkte & Ratgeber
         </div>
+        <p class="mb-4">
+          Trockentrenntoiletten erfreuen sich überall, wo wasserführende
+          Sanitärinstallationen nicht möglich, zunehmender Beliebtheit:
+        </p>
+        <div class="mb-4">
+          <ul>
+            <li>im Wohnmobil</li>
+            <li>im Gartenhaus</li>
+            <li>im Wochenend- oder Ferienhaus.</li>
+          </ul>
+        </div>
+        <p class="mb-4">
+          Wo Menschen zusammenkommen oder länger verweilen, ist es gut, eine
+          Toilette zu besitzen. Doch nicht immer lässt die Infrastruktur in
+          abgelegenen Regionen, auf Naturgrundstücken, in Wasserschutzgebieten
+          oder unterwegs Sanitärinstallationen zu.
+        </p>
+        <p class="mb-4">
+          Trockentrenntoiletten sind hier die optimale Lösung - sie kommen ganz
+          ohne Rohrleitungssystem und Wasseranschluss aus.
+        </p>
+        <p class="mb-4">
+          Moderne Trenn- und Trockentoiletten sind die umweltfreundliche,
+          hygienische und geruchsneutrale Alternative zu chemischen Toiletten.
+        </p>
+        <div class="mb-4 h2 pt-5">
+          Der Unterschied zwischen Trenn- und Trockentoiletten
+        </div>
+        <p class="mb-4">
+          Der Begriff Trockentrenntoilette setzt sich aus den Bezeichnungen
+          Trockentoilette und Trenntoilette zusammen:
+        </p>
+        <p class="mb-4 h4">Was ist eine Trockentoilette?</p>
+        <p class="mb-4">
+          Wie es der Name bereits andeutet: Trockentoiletten kommen ganz ohne
+          Wasser aus.
+        </p>
+        <p class="mb-4">
+          Ansonsten verfügen auch diese Toiletten über einen Sitz, einen Deckel
+          und eine Toilettenschüssel. Über bestimmte Systeme landen entweder
+          alle Hinterlassenschaften in einem Behälter, der dann geleert werden
+          muss – oder es findet eine Trennung statt. Hier beginnt bereits der
+          Übergang zur Trenntoilette.
+        </p>
+        <div class="mb-4 h4">Was zeichnet eine Trenntoilette aus?</div>
+        <p class="mb-4">
+          Hat eine Trockentoilette getrennte Behälter für Gelbes und Braunes,
+          ist sie gleichzeitig auch eine Trenntoilette.
+        </p>
+        <p class="mb-4">
+          Die Trennung ist sinnvoll, da die Vermischung von Ausscheidungen zu
+          unangenehmen Gerüchen führen kann. Da Menschen wesentlich mehr Urin
+          ausscheiden und dieser nicht ganz so schnell unangenehm riecht, kann
+          der Behälter in anderen Intervallen entsorgt werden. Während Urin in
+          die Natur gegossen werden kann, werden feste Ausscheidungen in der
+          Regel anders entsorgt.
+        </p>
+        <p class="mb-4">
+          In vielen Trenntoiletten wird Festes zusätzlich mit etwas
+          Naturmaterial abgedeckt, wodurch Gerüche deutlich reduziert werden.
+        </p>
+
+        <div class="mb-4 h4">Geeignet sind unter anderem:</div>
+
+        <div class="mb-4">
+          <ul>
+            <li>Kokosfaser</li>
+            <li>Rindenmulch</li>
+            <li>Erde</li>
+            <li>Katzenstreu</li>
+            <li>Holzspäne</li>
+          </ul>
+        </div>
+
+        <div class="mb-4 h5">
+          Merke: Eine Trockentrennoilette ist eine Kombination aus
+          Trockentoilette und Trenntoilette. Sie kommt ohne Wasser aus und hat
+          getrennte Auffangvorrichtungen für Urin und Kot.
+        </div>
+
+        <div class="mb-4 h2 pt-5">
+          Worauf muss beim Kauf einer Trockentrenntoilette geachtet werden?
+        </div>
+
+        <p class="mb-4">
+          Wichtigste Kaufkriterien sind die Größe, Funktionalität, die Art der
+          Behältnisse, Entnahme-, Reinigungs- und Entsorgungsmöglichkeiten sowie
+          das Zubehör. Trenntoiletten können kleine
+          Vollkunststoff-Konstruktionen sein, die großen Mülleimern ähneln. Es
+          gibt Trockentoiletten in Holzverkleidungen, fertige Klohäuschen,
+          Camping-Trenntoiletten oder Minitoiletten.
+        </p>
+
+        <p class="lead mb-4">
+          Bei uns finden Sie die beliebtesten Modelle, dazu Tipps und Tricks
+          sowie ausführliche Kaufratgeber zum Thema Trockentoilette und
+          Trenntoilette.
+        </p>
       </div>
     </div>
+  </div>
 </template>
