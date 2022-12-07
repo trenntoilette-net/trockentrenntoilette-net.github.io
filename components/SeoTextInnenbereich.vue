0766727e8bccf2ded59034305d45730aa5ce536f
trenntoilette-net <trockentrenntoilette-com@protonmail.com>
2022-12-07 12:47:33 +0100

    deploying

---
+++ components/SeoTextInnenbereich.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container pt-5">
+    <div class="row g-5">
+      <div class="col-lg-8 offset-lg-2 fadeInUp" style="min-height: 400px">
+        <h1 class="mb-4 h2">Trockentrenntoiletten für den Innenbereich</h1>
+        <p class="mb-4">
+          Trockentrenntoiletten sind eine hervorragende und hygienische
+          Möglichkeit, um eine Toilette unabhängig von Strom und Wasser nutzen
+          zu können. Sie sind vollkommen geruchlos und unkompliziert und können
+          entweder ohne weitere Zusätze, mit
+          <nuxt-link
+            to="/zubehoer/einstreu/"
+            title="Einstreu für Trockentrenntoiletten"
+            >Einstreu</nuxt-link
+          >
+          oder einem Abluftsystem betrieben werden.
+        </p>
+
+        <h2 class="h2">Was sind Trockentrenntoiletten für den Innenbereich?</h2>
+
+        <p class="mb-4">
+          Wer ein Garten- oder Tiny-Haus besitzt, das jedoch keinen Wasser- und
+          Stromanschluss hat, ist mit einer Trockentrenntoilette für den
+          Innenbereich am besten beraten. Flüssige und feste Ausscheidungen
+          werden dank Trennsystem voneinander getrennt und in separaten
+          Behältern aufgefangen, sodass keine unangenehmen Gerüche entstehen.
+        </p>
+
+        <p class="mb-4">
+          Für eine bessere Bindung von Feuchtigkeit kann es jedoch
+          empfehlenswert sein, Einstreu zu verwenden, die nach dem Toilettengang
+          einfach über die Ausscheidungen gegeben wird. Holzfasern eignen sich
+          diesbezüglich am besten, jedoch können auch Rindenmulch, Kokos- oder
+          Getreidefasern verwendet werden.
+        </p>
+
+        <p class="mb-4">
+          Wird die Trockentrenntoilette für den Innenbereich täglich genutzt,
+          kann sie auch mit einem Abluftsystem kombiniert werden, das sich ganz
+          bequem über eine Powerbank betreiben lässt, wenn kein Stromanschluss
+          vorhanden ist.
+        </p>
+
+        <h3 class="h2">Trockentrenntoilette für den Innenbereich Vorteile</h3>
+
+        <p class="mb-4">
+          Trockentrenntoiletten für den Innenbereich haben einen klaren Vorteil:
+          Sie brauchen weder Wasser noch Strom, um ihren Zweck als Toilette zu
+          erfüllen. Dies kann vor allem in Gartenhäusern von Vorteil sein, in
+          denen es weder Wasser- noch Stromanschluss gibt. Da die festen und
+          flüssigen Ausscheidungen voneinander getrennt werden, arbeitet die
+          Trockentrenntoilette für den Innenbereich vollkommen geruchlos.
+        </p>
+
+        <p class="mb-4">
+          Chemische Geruchsbinder sind für diese Toiletten somit nicht
+          notwendig. Nach dem Toilettengang kann allerdings etwas Einstreu
+          verwendet werden, um die Feuchtigkeit zu binden und den Inhalt im
+          heimischen Garten als Kompost zu verwenden. In diesem Fall sollten
+          unbedingt kompostierbare Tüten für die Trennbehälter verwendet werden.
+          Alternativ kann der Inhalt auch in den Restmüll gegeben werden.
+        </p>
+
+        <h4 class="h2">
+          Was beim Kauf einer Trockentrenntoilette für den Innenbereich
+          beachten?
+        </h4>
+
+        <p class="mb-4">
+          Im Innenbereich spielt der Komfort eine große Rolle. Aus diesem Grund
+          sollten Trockentrenntoiletten für den Innenbereich ein gutes
+          Sitzgefühl und eine einfache Handhabung bieten. Beispielsweise sollten
+          die Behälter leicht entleert werden können, ohne mit dem Inhalt in
+          Kontakt zu kommen. Wichtig ist auch die Stabilität des Materials,
+          damit sich Trockentrenntoiletten für den Innenbereich auch für
+          schwerere Personen eignen.
+        </p>
+
+        <h5 class="h2">
+          Fazit: Geruchlos und leicht zu handhaben: Trockentrenntoiletten für
+          den Innenbereich
+        </h5>
+
+        <p class="mb-4">
+          In vielen Gartenhäusern ist weder ein Wasseranschluss noch
+          Stromanschluss vorhanden. Hier sind Trockentrenntoiletten für den
+          Innenbereich eine hervorragende Alternative, um vollkommen autark und
+          unabhängig von Strom und Wasser zu sein. Durch die separaten
+          Trennbehälter sind Trockentrenntoiletten vollkommen geruchlos und
+          können entweder im Restmüll oder auf dem Kompost entsorgt werden.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/innenbereich.vue
+++ pages/innenbereich.vue
@@ -21,6 +21,7 @@
         </div>
       </div>
     </div>
+    <SeoTextInnenbereich />
   </div>
 </template>
 
