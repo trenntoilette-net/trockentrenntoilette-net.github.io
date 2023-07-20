c95cb77e98dd58c3fe1d0b251e5970c69e158b79
trenntoilette-net <trockentrenntoilette@proton.me>
2023-07-20 10:57:14 +0200

    deploying

---
+++ docs/_nuxt/static/1689843383/datenschutz/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1689843383",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdatenschutz\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"830038cd"}}}}(null));
---
+++ pages/trobolo-bilabox.vue
@@ -0,0 +1,298 @@
+<template>
+    <div>
+        <ProductHeader productName="TROBOLO BilaBox"
+            textContent="Die TROBOLO BilaBox ist eine mobile und leichte Trenntoilette im Eurobox-Format, die ideal für den flexiblen Einsatz unterwegs ist. Mit kompakten Maßen und einem Gewicht von nur 2,8 kg ist sie einfach zu transportieren und benötigt nur wenig Platz. Die BilaBox punktet durch ihre durchdachte Konstruktion, die Feststoffbehälter überflüssig macht. Stattdessen werden feste Ausscheidungen direkt in einem Inlay gesammelt, das den Vorteil hat, das Gewicht zu minimieren und das Fassungsvermögen zu erhöhen."
+            productImage="/assets/images/campingtoiletten/trobolo-bilabox/trobolo-bilabox.webp"></ProductHeader>
+
+        <!-- About Start -->
+        <div class="container-xxl py-5" id="testbericht">
+            <div class="container">
+                <div class="row g-5">
+                    <div class="col-lg-8 fadeInUp" style="min-height: 400px">
+                        <h2 class="mb-4">Testbericht</h2>
+                        <p class="mb-4">
+                            Wenn Sie eine preisgünstige Trenntoilette für unterwegs suchen, könnte die brandneue BilaBox
+                            genau das Richtige für Sie sein. Wir haben die Eurobox Toilette aus dem Hause TROBOLO für Sie
+                            getestet und zeigen Ihnen in unserem Testbericht, was die neue Trenntoilette im Euroboxformat zu
+                            bieten hat.
+                        </p>
+
+                        <p class="mb-4">
+                            Dabei schauen wir uns genau an, wie gut die BilaBox wirklich ist und wo ihre Stärken und
+                            Schwächen liegen. Und wie schneidet sie im direkten Vergleich zur Boxio Toilette ab, die auf den
+                            ersten Blick sehr ähnlich wirkt?
+                        </p>
+
+                        <p class="mb-4">Lesen Sie weiter und finden Sie heraus, ob die BilaBox die passende Trenntoilette
+                            für Sie ist.</p>
+
+                        <ImageGallerySlider :images="product.localImages" :alt="product.name" />
+
+
+                        <h5 class="mb-3 mt-5">Komfort</h5>
+
+                        <p>Wer sich für eine Eurobox Toilette entscheidet, sucht in der Regel eine Toilette, die besonders
+                            für den flexiblen, mobilen Einsatz geeignet ist. Beim Komfort kommt es also weniger darauf an,
+                            wie bequem man sitzt, sondern wie komfortabel die Trenntoilette für ihren Verwendungszweck
+                            eingesetzt werden kann.</p>
+
+                        <p>Die BilaBox bietet dank kompakten Eurobox-Maßen von 297x400x300 mm (HTB) ideale Voraussetzungen
+                            für eine mobile Trenntoilette, da sie nur wenig Platz benötigt. Sie lässt sich somit leicht
+                            unterbringen und beispielsweise im Kofferraum verstauen. Für die kompakten Maße gibt es also den
+                            ersten Pluspunkt in Sachen Komfort für die BilaBox.</p>
+
+                        <p>Mit nur 2,8 kg ist die BilaBox zudem die leichteste Trenntoilette, die uns aktuell bekannt ist.
+                            Die BilaBox ist trotz vergleichbarer Größe sogar ein ganzes Kilo leichter als die 3,9 kg schwere
+                            Boxio Trenntoilette. Dank dem geringen Gewicht kann man die BilaBox ganz einfach hochheben,
+                            tragen und umstellen. Die Einkerbungen am Rand der BilaBox dienen als Tragegriff und sorgen für
+                            angenehmen Tragekomfort.</p>
+
+                        <p>Interessant ist, dass die BilaBox gänzlich ohne Feststoffbehälter auskommt. Die festen
+                            Ausscheidungen landen ganz einfach in einem Inlay, das am Separator angebracht wird. Die Box
+                            fungiert dabei selbst als Feststoffbehälter. Das ist eine ziemlich clevere Lösung, denn wieso
+                            sollte man einen Behälter aus Kunststoff in eine Box aus Kunststoff stellen, wenn die Box selbst
+                            bereits den gleichen Zweck erfüllen kann? Diese Lösung minimiert nicht nur das Gewicht, sondern
+                            erhöht gleichzeitig auch die Kapazität für Feststoffe. So schafft die BilaBox es auf ein
+                            Fassungsvermögen von 9 Litern, während die Boxio Trenntoilette mit Feststoffbehälter ein
+                            deutlich geringeres Fassungsvermögen von nur 5,7 Litern aufweist.</p>
+
+                        <p>Ein weiteres Feature, das uns gefällt, ist der Toilettendeckel. Dieser lässt sich nämlich ganz
+                            einfach am hinteren Teil der BilaBox einstecken. Bei der Trenntoilette von Boxio muss der
+                            Toilettendeckel umständlich über eine Schnur befestigt werden, die man relativ mühsam selbst
+                            anbringen muss. Auch hier hat die BilaBox unserer Meinung nach also ganz klar die Nase vorn.</p>
+
+                        <h5 class="mb-3 mt-5">Eckdaten</h5>
+
+                        <p>Die BilaBox ist 2,8 kg leicht und misst kompakte 297x400x300 mm (HTB) bei einer Sitzhöhe von 286
+                            mm. Damit passt die BilaBox in jeden Kofferraum und lässt genügend Platz für Koffer, Taschen und
+                            andere Dinge, die Sie in Ihrem Kofferraum unterbringen möchten.</p>
+
+                        <p>Die Verarbeitung der BilaBox macht einen stabilen Eindruck und es wird eine getestete Traglast
+                            von 150 kg angegeben. Wer etwas mehr wiegt, muss sich wahrscheinlich auch keine Sorgen machen.
+                        </p>
+
+                        <h5 class="mb-3 mt-5">
+                            Funktionsweise, Material und Geruchsentwicklung
+                        </h5>
+
+                        <p>Bei der BilaBox handelt es sich um eine Trenntoilette. Das heißt, dass feste und flüssige
+                            Ausscheidungen bei der Toilettennutzung voneinander getrennt werden. Damit diese Trennung
+                            effektiv funktionieren kann, findet sich im Inneren der BilaBox ein Separator, der zuverlässig
+                            dafür sorgt, dass die flüssigen Ausscheidungen in den Flüssigkeitsbehälter geleitet werden,
+                            während feste Ausscheidungen im Inlay im hinteren Teil der Trenntoilette landen.</p>
+                        <p>Der Trennmechanismus hat in unserem Praxistest einwandfrei funktioniert. Wichtig ist, dass man
+                            die Trenntoilette, wie jede Trenntoilette, nur im Sitzen benutzen sollte. Übrigens ist uns
+                            aufgefallen, dass der Trenneinsatz der BilaBox deutlich hochwertiger und stabiler wirkt als der
+                            Trenneinsatz von Boxio, der sich recht wabbelig anfühlt.</p>
+
+                        <p>Die BilaBox kann dank Trenneinsatz, Einstreu und TROBOLO SafeShell System gänzlich geruchlos
+                            genutzt werden. Die Trennung der Ausscheidungen minimiert die Geruchsentwicklung bereits
+                            dadurch, dass die Vermischung von Urin und Kot verhindert wird. Damit die festen Ausscheidungen
+                            gänzlich geruchlos bleiben, sollte nach jedem großen Toilettengang natürliche Einstreu in das
+                            Inlay für Feststoffe gegeben werden. Zusätzlich verhindert das SafeShell System, dass
+                            unangenehme Uringerüche aus dem Flüssigkeitsbehälter entweichen können. So bleibt der
+                            Flüssigkeitsbehälter jederzeit vor Gerüchen geschützt. Theoretisch könnte man den Urin im
+                            Flüssigkeitsbehälter also einige Wochen, Monate oder sogar noch länger geruchlos im
+                            Flüssigkeitsbehälter aufbewahren. Zusätzlich sorgt das SafeShell System dafür, dass während der
+                            Fahrt keine Flüssigkeiten ausschwappen können.</p>
+
+                        <p>Die Entleerung der Ausscheidungen ist kinderleicht. Durch die Trennung der Ausscheidungen können
+                            flüssige und feste Ausscheidungen getrennt voneinander entnommen und entsorgt werden. Den
+                            Flüssigkeitsbehälter können Sie über eine reguläre Toilette abgießen oder mit Wasser verdünnen
+                            und als Naturdünger nutzen. Die festen Ausscheidungen können Sie samt Inlay in den Hausmüll
+                            geben.</p>
+
+                        <p>Theoretisch wäre auch eine Kompostierung der festen Ausscheidungen möglich. Hierfür ist es jedoch
+                            notwendig, dass Sie ein Inlay aus kompostierbarem Material nutzen. Da das Inlay bei der BilaBox
+                            direkt am Trenneinsatz angebracht wird und es keinen eigenen Feststoffbehälter gibt, raten wir
+                            jedoch davon ab, bei der BilaBox auf kompostierbare Inlays zurückzugreifen.</p>
+
+                        <p>Beim Material der BilaBox setzt TROBOLO auf Polypropylen, einen robusten Kunststoff, der Wind und
+                            Wetter trotzt. So ist sichergestellt, dass Sie sehr lange Spaß an Ihrer BilaBox haben können.
+                        </p>
+
+                        <h5 class="mb-3 mt-5">Nachhaltigkeit</h5>
+
+                        <p>Wer Wert auf Nachhaltigkeit legt, trifft mit der BilaBox eine gute Wahl. Das Produkt wird aus
+                            langlebigem Polypropylen gefertigt und alle Einzelteile, wie beispielsweise der
+                            Flüssigkeitsbehälter, können bei Bedarf ausgetauscht werden.</p>
+
+                        <p>Zusätzlich erfordert der Betrieb der BilaBox weder Wasser, Strom noch Chemie, was ebenfalls der
+                            nachhaltigen Entwicklung zuträglich ist.</p>
+
+                        <h5 class="mb-3 mt-5">Zubehör</h5>
+
+                        <p>Für den Betrieb der BilaBox sollten Sie Einstreu und Kunststoff-Inlays verwenden. Sowohl die
+                            Einstreu als auch die passenden Inlays können Sie direkt vom Hersteller TROBOLO beziehen.</p>
+
+                        <p>Wenn Sie lieber Alternativen von anderen Herstellern oder beispielsweise Sägespäne aus eigener
+                            Herstellung oder Kleintierstreu verwenden möchten, ist dies auch problemlos möglich.</p>
+
+                        <p>Bei der Einstreu sollten Sie darauf achten, dass diese eine ähnliche Konsistenz aufweist, wie die
+                            Einstreu von TROBOLO. Ist die Einstreu zu fein oder zu grob, wirkt sich dies negativ auf die
+                            Effektivität aus. Weiteres Zubehör wird bei der BilaBox nicht benötigt.</p>
+
+                        <h5 class="mb-3 mt-5">Fazit</h5>
+
+                        <p>Die TROBOLO BilaBox ist ideal, wenn Sie nach einer mobilen Toilette für unterwegs suchen, die Sie
+                            überall hin mitnehmen und an jedem Ort nutzen können. Für den Preis finden wir die BilaBox
+                            einfach unschlagbar.</p>
+
+                        <p>Neben dem Preis sind das geringe Gewicht und das integrierte TROBOLO SafeShell System für uns die
+                            Highlights an der BilaBox. Im Großen und Ganzen gefällt uns die BilaBox auch ganz klar besser
+                            als die Boxio.</p>
+
+                        <ImageGallery :images="product.localImages" :alt="product.name" />
+
+                        <div class="row gy-2 gx-4 mb-5 mt-5">
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Funktionalität
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Materialien
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Nachhaltigkeit
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Geruchsentwicklung
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Komfort
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Entsorgung & Hygiene
+                                </p>
+                            </div>
+                        </div>
+                        <a class="btn btn-primary py-3 px-5" target="_blank" rel="nofollow noopener"
+                            :href="product.shopLink">Bestellen</a>
+                    </div>
+                    <div class="col-lg-4 fadeInUp">
+                        <ProductCard :product="product" />
+                        <div class="mt-5">
+                            <a class="btn btn-primary py-3 px-5" target="_blank" rel="nofollow noopener"
+                                :href="product.shopLink" style="display: block; width: 100%">{{ product.brand }}
+                                Online Shop</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+    </div>
+</template>
+  
+<script>
+import products from "~/assets/products.json";
+
+const product = products.find((item) => item.name === "TROBOLO BilaBox");
+
+export default {
+    name: "TROBOLOBilabox",
+    head: {
+        title: "Trobolo Bilabox - Mobile Trenntoilette im Test",
+        meta: [
+            {
+                hid: "description",
+                name: "description",
+                content:
+                    "Die Trobolo Bilabox ist eine mobile Trenntoilette, die sich für den Einsatz im Van, im Boot, im PKW oder beim Camping eignet. Wir haben die Trenntoilette getestet und verraten dir, ob sie sich lohnt.",
+            },
+            {
+                hid: "robots",
+                name: "robots",
+                content: product.robots
+            }
+        ],
+    },
+    asyncData: () => {
+        return {
+            product: products.find((item) => item.name === "TROBOLO BilaBox"),
+        };
+    },
+    jsonld() {
+        return {
+            "@context": "https://schema.org",
+            "@graph": [
+                {
+                    "@context": "https://schema.org/",
+                    "@type": "Product",
+                    name: this.product.name,
+                    image: [
+                        "https://www.trockentrenntoilette.net/assets/images/campingtoiletten/campingtoilette-trobolo-bilabox.webp",
+                    ],
+                    description:
+                        "Trobolo Bilabox ultraleichte Trenntoilette für den mobilen Einsatz. Die Trenntoilette ist für den Einsatz im Van, im Boot, im PKW oder beim Camping geeignet.",
+                    sku: "TroboloBilabox",
+                    brand: {
+                        "@type": "Brand",
+                        name: this.product.brand,
+                    },
+                    review: {
+                        "@type": "Review",
+                        reviewRating: {
+                            "@type": "Rating",
+                            ratingValue: this.product.stars,
+                            bestRating: this.product.stars,
+                        },
+                        author: {
+                            "@type": "Organization",
+                            name: "Trockentrenntoilette.net",
+                        },
+                    },
+                    aggregateRating: {
+                        "@type": "AggregateRating",
+                        ratingValue: "4.5",
+                        reviewCount: "1",
+                    },
+                    offers: {
+                        "@type": "Offer",
+                        url: "https://www.trockentrenntoilette.net/trobolo-bilabox/",
+                        priceCurrency: "EUR",
+                        price: "599.00",
+                        priceValidUntil: "2024-11-20",
+                        itemCondition: "https://schema.org/NewCondition",
+                        availability: "https://schema.org/InStock",
+                    },
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "BreadcrumbList",
+                    itemListElement: [
+                        {
+                            "@type": "ListItem",
+                            position: 1,
+                            item: {
+                                "@id": "https://www.trockentrenntoilette.net/",
+                                name: "Trockentrenntoiletten",
+                            },
+                        },
+                        {
+                            "@type": "ListItem",
+                            position: 2,
+                            item: {
+                                "@id": "https://www.trockentrenntoilette.net/trobolo-bilabox/",
+                                name: this.product.name,
+                            },
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+};
+
+</script>
+  
