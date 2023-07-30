f098856537f57b340aa925a6220f744ebeebf2fb
trenntoilette-net <trockentrenntoilette@proton.me>
2023-07-30 21:18:27 +0200

    deploying

---
+++ pages/trobolo-terago.vue
@@ -0,0 +1,252 @@
+<template>
+    <div>
+        <ProductHeader productName="TROBOLO TeraGO"
+            textContent="Entdecken Sie die TROBOLO TeraGO, eine revolutionäre Campingtoilette, die ein unvergleichliches Gleichgewicht zwischen Komfort und Nachhaltigkeit bietet. Mit ihrem leichten Design und der effizienten Abfalltrennung ist sie Ihr perfekter Begleiter für jedes Outdoor-Abenteuer. Lesen Sie unseren Testbericht und erfahren Sie mehr über die TeraGO von TROBOLO."
+            productImage="/assets/images/campingtoiletten/trobolo-terago/trobolo-terago.webp"></ProductHeader>
+
+        <!-- About Start -->
+        <div class="container-xxl py-5" id="testbericht">
+            <div class="container">
+                <div class="row g-5">
+                    <div class="col-lg-8 fadeInUp" style="min-height: 400px">
+                        <h2 class="mb-4">Testbericht</h2>
+                        <p class="mb-4">Wir haben die TROBOLO TeraGO in die Hand genommen und dieses tragbare WC auf die
+                            Probe gestellt. In dieser Produktbewertung teilen wir unsere Erfahrungen und untersuchen, ob die
+                            TeraGO ihrem Ruf als ultimatives Camping-WC gerecht wird. Lasst uns eintauchen!</p>
+
+                        <!-- <ImageGallerySlider :images="product.localImages" alt="TROBOLO WandaGo Campingtoilette" /> -->
+
+                        <h5 class="mb-3 mt-5">Komfort</h5>
+
+                        <p>Der Komfort ist ein weiterer Bereich, in dem die TeraGO glänzt. Die ergonomisch geformte
+                            Toilettenbrille bietet einen hohen Komfort, sodass Sie das WC über längere Zeiträume hinweg ohne
+                            Unbehagen oder Anstrengung nutzen können. Ob Sie in der Wildnis campen oder die freie Natur
+                            erkunden, die TeraGO sorgt dafür, dass Ihre Toilettenerfahrung angenehm und praktisch bleibt.
+                        </p>
+                        <p>Die TeraGO ist mit Blick auf die Praktikabilität konzipiert. Der Toilettendeckel schließt fest,
+                            um unerwünschte Gerüche oder Leckagen während des Transports zu verhindern. Zusätzlich
+                            erleichtern die hilfreichen Tragegriffe das Bewegen der TeraGO, was ihre Tragbarkeit weiter
+                            erhöht.</p>
+
+                        <h5 class="mb-3 mt-5">Eckdaten</h5>
+
+                        <p>Eine der herausragenden Eigenschaften der TROBOLO TeraGO ist ihre einfache Montage. Ohne
+                            zusätzliches Werkzeug können Sie dieses tragbare WC schnell aufbauen und einsatzbereit machen.
+                            Dieser Komfort wird besonders geschätzt, wenn Sie unterwegs sind und ein unkompliziertes
+                            Erlebnis wünschen.</p>
+
+                        <p>Das kompakte Design der TeraGO ermöglicht es ihr, nahtlos in die Staufächer aller
+                            Standard-Campervans zu passen. Ob Sie ein Wochenend-Getaway planen oder eine lange Reise
+                            unternehmen, Sie können sicher sein, dass die TeraGO keinen unnötigen Platz einnimmt. Ihre
+                            Abmessungen sind sorgfältig optimiert, um die Speichereffizienz zu maximieren, ohne dabei die
+                            Funktionalität zu beeinträchtigen.</p>
+
+                        <p>Mit einem Gewicht von nur 6,8 kg ist die TeraGO unglaublich leicht und daher einfach zu
+                            transportieren. Ob Sie sie von Ihrem Fahrzeug zu Ihrem Campingplatz bewegen müssen oder sie auf
+                            Outdoor-Ausflüge mitnehmen möchten, die Tragbarkeit der TeraGO ist ein großer Vorteil.</p>
+
+                        <h5 class="mb-3 mt-5">
+                            Funktionsweise, Material und Geruchsentwicklung
+                        </h5>
+
+                        <p>Die zuverlässige Trennung von Urin und Feststoffen ist ein Schlüsselmerkmal der TeraGO. Während
+                            unserer Tests stellten wir fest, dass der Trennmechanismus einwandfrei funktioniert und effektiv
+                            verhindert, dass sich Abfälle vermischen. Dies verbessert nicht nur die Hygiene, sondern trägt
+                            auch zur Geruchskontrolle bei.</p>
+
+                        <p>In Bezug auf die Geruchskontrolle nutzt die TeraGO das TROBOLO SafeShell System®, welches dafür
+                            sorgt, dass das WC stets geruchsneutral und auslaufsicher bleibt. Dies ist besonders wichtig in
+                            engen Räumen, wie z.B. Campervans oder Zelten, wo unangenehme Gerüche schnell überwältigend
+                            werden können. Mit der TeraGO können Sie ein frisches und hygienisches WC-Erlebnis ohne Sorgen
+                            genießen.</p>
+
+                        <p>Das Entleeren und Reinigen der TeraGO ist ein Kinderspiel. Der praktische Halter für den
+                            Urinumleiter im Deckel sorgt für Komfort und stellt sicher, dass während des Transports alles an
+                            seinem Platz bleibt. Wenn es an der Zeit ist, die Abfallbehälter zu entleeren, ermöglicht das
+                            Design der TeraGO eine einfache und hygienische Entnahme. Sie können den Abfall schnell und ohne
+                            Schmutz oder Aufwand entsorgen.</p>
+                        <p>
+                            Die Entnahme der Behälter ist denkbar einfach. Man muss lediglich
+                            den Toilettensitz inklusive Trenneinsatz anheben und kann diesen
+                            bequem im Toilettendeckel einrasten lassen, während man die
+                            Behälter komfortabel und hygienisch zur Entleerung entnimmt.
+                        </p>
+
+                        <h5 class="mb-3 mt-5">Nachhaltigkeit</h5>
+
+                        <p>Die TROBOLO TeraGO ist eine autarke Komposttoilette, das heißt, sie funktioniert ohne Wasser,
+                            Chemikalien oder Strom. Dieser umweltfreundliche Ansatz gewährleistet, dass Sie die TeraGO
+                            jederzeit und überall nutzen können, ohne auf externe Ressourcen angewiesen zu sein.</p>
+
+                        <h5 class="mb-3 mt-5">Zubehör</h5>
+
+                        <p>Um Ihre Erfahrung mit der TROBOLO TeraGO zu verbessern, sind optionales Zubehör erhältlich.
+                            Einlagen und ein Toilettenpapierspender können separat erworben werden und ermöglichen es Ihnen,
+                            Ihr tragbares WC-Setup vollständig anzupassen. Diese Accessoires erhöhen den Komfort und
+                            erleichtern die Wartung noch mehr.</p>
+
+                        <h5 class="mb-3 mt-5">Fazit</h5>
+
+                        <p>Zusammenfassend ist die TROBOLO TeraGO ein fantastisches Camping-WC, das Funktionalität,
+                            Tragbarkeit und Komfort vereint. Die einfache Montage, das kompakte Design, die leichte Bauweise
+                            und der autarke Betrieb machen sie zur ausgezeichneten Wahl für Outdoor-Enthusiasten. Mit den
+                            zuverlässigen Funktionen der TeraGO zur Abfalltrennung, Geruchskontrolle und hygienischen
+                            Wartung können Sie ein komfortables und angenehmes WC-Erlebnis genießen, egal wohin Ihre
+                            Abenteuer Sie führen. Die TeraGO setzt neue Maßstäbe bei tragbaren WCs und ist für Camper,
+                            Roadtrippers und alle, die eine zuverlässige und umweltfreundliche WC-Lösung suchen, sehr zu
+                            empfehlen.</p>
+
+                        <!-- <ImageGallery :images="product.localImages" alt="TROBOLO WandaGO Campingtoilette" /> -->
+
+                        <div class="row gy-2 gx-4 mb-5 mt-5">
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Funktionalität
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Materialien
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Nachhaltigkeit
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Geruchsentwicklung
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Komfort
+                                </p>
+                            </div>
+                            <div class="col-sm-6">
+                                <p class="mb-0">
+                                    <i class="fa fa-check text-info me-2"></i>Entsorgung & Hygiene
+                                </p>
+                            </div>
+                        </div>
+                        <a class="btn btn-primary py-3 px-5" target="_blank" rel="nofollow noopener"
+                            :href="product.shopLink">Bestellen</a>
+                    </div>
+                    <div class="col-lg-4 fadeInUp">
+                        <ProductCard :product="product" />
+                        <div class="mt-5">
+                            <a class="btn btn-primary py-3 px-5" target="_blank" rel="nofollow noopener"
+                                :href="product.shopLink" style="display: block; width: 100%">TROBOLO Online Shop</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+    </div>
+</template>
+  
+<script>
+import products from "~/assets/products.json";
+
+const product = products.find((item) => item.name === "TROBOLO TeraGO");
+
+export default {
+    name: "TeraGO",
+    head: {
+        title: "TROBOLO®TeraGO: Hochwertige Campingtoilette im ausführlichen Test",
+        meta: [
+            {
+                hid: "description",
+                name: "description",
+                content:
+                    "Testbericht zur TROBOLO TeraGO Campingtoilette. Kombiniert Komfort, Mobilität und Nachhaltigkeit für Ihre Outdoor-Abenteuer.",
+            },
+            {
+                hid: "robots",
+                name: "robots",
+                content: product.robots
+            }
+        ],
+    },
+    asyncData: () => {
+        return {
+            product: products.find((item) => item.name === "TROBOLO TeraGO"),
+        };
+    },
+    jsonld() {
+        return {
+            "@context": "https://schema.org",
+            "@graph": [
+                {
+                    "@context": "https://schema.org/",
+                    "@type": "Product",
+                    name: "TROBOLO TeraGO",
+                    image: [
+                        "https://www.trockentrenntoilette.net/assets/images/campingtoiletten/trobolo-terago/trobolo-terago.webp",
+                        // Weitere Bilder können hier hinzugefügt werden
+                    ],
+                    description:
+                        "TROBOLO TeraGO - Eine leichte und portable Campingtoilette, die Komfort und Funktionalität vereint.",
+                    sku: "P1XX1403",
+                    mpn: "48648",
+                    brand: {
+                        "@type": "Brand",
+                        name: "TROBOLO",
+                    },
+                    review: {
+                        "@type": "Review",
+                        reviewRating: {
+                            "@type": "Rating",
+                            ratingValue: "5",
+                            bestRating: "5",
+                        },
+                        author: {
+                            "@type": "Organization",
+                            name: "Trockentrenntoilette.net",
+                        },
+                    },
+                    aggregateRating: {
+                        "@type": "AggregateRating",
+                        ratingValue: "5",
+                        reviewCount: "354",
+                    },
+                    offers: {
+                        "@type": "Offer",
+                        url: "https://trobolo.com/de/produkte/terago/",
+                        priceCurrency: "EUR",
+                        price: "299.00",
+                        priceValidUntil: "2024-11-20",
+                        itemCondition: "https://schema.org/NewCondition",
+                        availability: "https://schema.org/InStock",
+                    },
+                },
+                {
+                    "@context": "https://schema.org",
+                    "@type": "BreadcrumbList",
+                    itemListElement: [
+                        {
+                            "@type": "ListItem",
+                            position: 1,
+                            item: {
+                                "@id": "https://www.trockentrenntoilette.net/",
+                                name: "Trockentrenntoiletten",
+                            },
+                        },
+                        {
+                            "@type": "ListItem",
+                            position: 2,
+                            item: {
+                                "@id": "https://www.trockentrenntoilette.net/trobolo-terago/",
+                                name: "TROBOLO TeraGO",
+                            },
+                        },
+                    ],
+                },
+            ],
+        };
+    },
+};
+</script>
+  
